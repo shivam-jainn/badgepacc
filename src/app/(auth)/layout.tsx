d717a53d234dceaedefc64f61817bfdea61fc73f
shivam-jainn <shivam.clstash@gmail.com>
2024-04-14 20:07:55 +0530

    fix((auth) folder): :bug: Fixed overflow of layout
    nada
    
    nada

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function layout({children}:{children:ReactNode}) {
   return (
     <section className='w-full h-screen bg-[#131517] '>
         
-        <div className='bg-gradient-anim-auth bg-gradient-auth w-full h-full absolute bottom-0  blur-10 '>
+        <div className='bg-gradient-anim-auth bg-gradient-auth w-full h-full flex items-center align-middle  blur-10 '>
         {children}
         </div>
     </section>
