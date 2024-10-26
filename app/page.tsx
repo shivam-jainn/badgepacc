854c9abefb9419e558a3a52951924d069a2c1a8b
shivam-jainn <shivam.clgstash@gmail.com>
2024-10-26 09:39:44 -0400

    init : removed yarn , starter components , cleanup!

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { Welcome } from '../components/Welcome/Welcome';
-
 export default function HomePage() {
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+     Hello Mantine
     </>
   );
 }
