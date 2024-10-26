2033321524c299eaa61873fa719663b5b7bdee49
shivam-jainn <shivam.clgstash@gmail.com>
2024-10-26 11:42:50 -0400

    feat(auth): :construction: Configured auth options,middleware and other minor things

---
+++ auth.config.ts
@@ -0,0 +1,51 @@
+import GitHub from "next-auth/providers/github"
+import type { NextAuthConfig } from "next-auth"
+import Discord from "next-auth/providers/discord"
+import Github from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+
+export default {
+    providers: [
+        Github,
+        Google,
+        Discord
+      ],
+      pages: {
+        signIn: '/signin',
+        newUser: '/onboarding/username'
+      },
+      callbacks: {
+        authorized({ request: { nextUrl }, auth }) {
+          const isLoggedIn = !!auth?.user;
+          const { pathname } = nextUrl;
+          const isOrg = auth?.user.isOrg || 'user';
+
+          if (pathname.startsWith('/signin') && isLoggedIn) {
+              return Response.redirect(new URL('/', nextUrl));
+          }
+    
+    
+          if (pathname.startsWith("/org") && !isOrg) {
+              return Response.redirect(new URL('/', nextUrl));
+          }
+    
+          return !!auth;
+        },
+        jwt({ token, user, trigger, session }) {
+          if (user) {
+            token.id = user.id as string;
+            token.isOrg = user.isOrg as boolean;
+          }
+          if (trigger === "update" && session) {
+            token = { ...token, ...session };
+          }
+    
+          return token;
+        },
+        session({ session, token }) {
+          session.user.id = token.id;
+          session.user.isOrg = token.isOrg;
+          return session;
+        }
+      }
+} satisfies NextAuthConfig
